f676981ec646b0c69d0e7f1224689bf30705f894
pisa-kun <pisa0401mst@gmail.com>
2024-08-13 00:27:31 +0900

    add compute engine(gce)

--- chapter2/main.tf
+++ chapter2/main.tf
@@ -17,4 +17,36 @@ provider "google" {
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network-test"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "my-custom-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  network       = google_compute_network.vpc_network.id
+}
+
+# Create a single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "flask-vm"
+  machine_type = "n2-standard-2"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  # Install Flask
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.id
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
 }
