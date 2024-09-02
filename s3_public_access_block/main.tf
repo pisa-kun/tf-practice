559bf05a1c93d2a5c0bbc2e826b84237f4222332
pisa-kun <pisa0401mst@gmail.com>
2024-09-02 15:34:34 +0900

    fix: 明示的にblock

--- s3_public_access_block/main.tf
+++ s3_public_access_block/main.tf
@@ -17,10 +17,49 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+## デフォルトで今はパブリックアクセスブロックはすべて無効化されている
 resource "aws_s3_bucket" "raw-sample" {
   bucket = "sample-bucket-raw-access-sumisumi" // s3バケット名をユニークにするために自分の名前など入れる
 }
 
+## 明示的に無効化する場合
 resource "aws_s3_bucket" "block-sample" {
   bucket = "sample-bucket-block-access-sumisumi" // s3バケット名をユニークにするために自分の名前など入れる
+}
+
+# バケットポリシーの内容を定義
+# see: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document
+data "aws_iam_policy_document" "main" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = [
+      aws_s3_bucket.block-sample.arn,
+      "${aws_s3_bucket.block-sample.arn}/*",
+    ]
+  }
+}
+
+# バケットポリシーをバケットに紐づける
+# see: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_policy
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.block-sample.id
+  policy = data.aws_iam_policy_document.main.json
+
+  // 依存順番を指定
+  depends_on = [ aws_s3_bucket_public_access_block.main,]
+}
+
+## falseの場合はアクセスできてしまう
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket                  = aws_s3_bucket.block-sample.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
